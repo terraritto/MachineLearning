3b021793cf54e952e4b6e56bdb5ac64018377176
terraritto <28126083+terraritto@users.noreply.github.com>
2019-12-01 01:24:44 +0900

    add fandemental auto encoder.

---
+++ autoencoder/underComplete.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import numpy as np
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+
+encodingDim = 32
+inputImg = Input(shape=(784,))
+encoded = Dense(encodingDim, activation='relu')(inputImg)
+decoded = Dense(784,activation='sigmoid')(encoded)
+autoencoder = Model(input=inputImg,output=decoded)
+
+autoencoder.compile(optimizer = 'adadelta', loss = 'binary_crossentropy')
+
+(x_train, _), (x_test, _) = mnist.load_data()
+x_train = x_train.astype('float32')/255.
+x_test = x_test.astype('float32')/255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+autoencoder.fit(x_train,x_train,
+        nb_epoch=50,
+        batch_size=256,
+        shuffle=True,
+        validation_data=(x_test,x_test)
+)
+
+autoencoder.save_weights('autoencoder.h5')
+
+decoded_imgs = autoencoder.predict(x_test)
+
+n = 10
+plt.figure(figsize=(20,4))
+for i in range(n):
+    ax = plt.subplot(2,n,i+1)
+    plt.imshow(x_test[i].reshape(28,28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    ax = plt.subplot(2,n,i+1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28,28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
